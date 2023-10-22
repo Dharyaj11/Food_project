70a99f2d6813bdcc5dcaf3159bbd636e155d13e9
Dharyaj11 <dharyajasuja2003@gmail.com>
2023-10-23 00:20:28 +0530

    added validator

--- mernapp/backend/Routes/CreatUser.js
+++ mernapp/backend/Routes/CreatUser.js
@@ -1,8 +1,18 @@
 const express=require('express')
 const router=express.Router()
 const User=require('../models/User')
+const { body, validationResult } = require('express-validator');
+router.post("/creatuser",
+[// username must be an email
+body('email').isEmail(),
+// password must be at least 5 chars long
+body('password').isLength({ min: 5 }),
+body('name').isLength({ min: 3 }),
+],async(req,res)=>{
 
-router.post("/creatuser",async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
     try {
         await User.create({
             name:req.body.name,
--- mernapp/backend/models/User.js
+++ mernapp/backend/models/User.js
@@ -23,6 +23,7 @@ const UserSchema =new Schema({
         default:Date.now
     }
     
+    
 });
 
 module.exports=mongoose.model('user',UserSchema)
