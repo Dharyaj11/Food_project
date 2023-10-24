3c0f271dca4fe43814c6a7c18c91db1dbc3d9fc9
Dharyaj11 <dharyajasuja2003@gmail.com>
2023-10-24 20:39:59 +0530

    added authToken for session

--- mernapp/backend/Routes/CreatUser.js
+++ mernapp/backend/Routes/CreatUser.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
-
+const jwt=require("jsonwebtoken");
+const jwtSecret="Dharyajasujawillgethighestpackageincollege$#"
 const bcrypt=require("bcrypt");
 router.post("/creatuser",
     [// username must be an email
@@ -53,16 +54,25 @@ router.post("/loginuser",
             if (!userData) {
                 return res.status(400).json({ errors: "try login with valid credentials" });
             }
-
-            if (req.body.password !== userData.password) {
+            const pwdCompare=await bcrypt.compare(req.body.password,userData.password)
+            if (!pwdCompare) {
                 return res.status(400).json({ errors: "try login with valid credentials" });
 
 
             }
             else {
-                return res.json({ success: true });
+
+                const data={
+                    user:{
+                        id:userData.id
+                    }
+                }
+                const authToken=jwt.sign(data,jwtSecret);
+                return res.json({ success: true,authToken:authToken });
 
             }
+
+
 
         } catch (error) {
             console.log(error);
--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -26,6 +26,8 @@ let navigate=useNavigate();
             alert("Enter valid credentials ");
         }
         if(json.success){
+          localStorage.setItem("authToken",json.authToken);
+          console.log(localStorage.getItem("authToken"));
            navigate('/');
         }
 
